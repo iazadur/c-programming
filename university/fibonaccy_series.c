18f7cbdf5d814e8b292c4766072445b7abe85373
iazadur <iamazadur@gmail.com>
2023-04-30 21:18:24 +0600

    all done

--- university/fibonaccy_series.c
+++ university/fibonaccy_series.c
@@ -1,1 +1,19 @@
-// it will comming soon 😊
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a = 0, b = 1;
+    printf("%d %d ", a, b);
+    for (int i = 0; i < n - 2; i++)
+    {
+        int c = a + b;
+        printf("%d ", c);
+        a = b;
+        b = c;
+    }
+
+    
+   return 0;
+}
